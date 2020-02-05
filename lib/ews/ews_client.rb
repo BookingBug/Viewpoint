15e2efb2b5363bc0affc8f691659dc06da85c39f
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2020-02-05 09:48:25 +0000

    Develop => Master  (#61)
    * Allow nil username/password
    
    * Remove simplecov rails
    
    * Remove useless cops

--- lib/ews/ews_client.rb
+++ lib/ews/ews_client.rb
@@ -41,8 +41,8 @@ class Viewpoint::EWSClient
   def initialize(endpoint, username, password, opts = {})
     # dup all. @see ticket https://github.com/zenchild/Viewpoint/issues/68
     @endpoint = endpoint.dup
-    @username = username.dup
-    password  = password.dup
+    @username = username&.dup
+    password  = password&.dup
     opts      = opts.dup
     http_klass = opts[:http_class] || Viewpoint::EWS::Connection
     con = http_klass.new(endpoint, opts[:http_opts] || {})
